3120fb0449e659c0d642d8f95753e68ad881888d
David Hayes <drhayes@gmail.com>
2010-10-02 00:42:11 -0700

    defining TileManager class

---
+++ src/tilemanager.js
@@ -0,0 +1,14 @@
+// tilemanager.js
+//
+// Manages a group of tiles retrievable by tile x,y.
+// This is not the same as entity x,y -- less granular.
+
+(function(global, $) {
+  
+  var TileManager = function() {
+    
+  };
+  
+  global.TileManager = TileManager;
+  
+})(window, jQuery)
