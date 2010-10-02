5401788db7bc7bba1ed02720ce98ae716e83bce0
David Hayes <drhayes@gmail.com>
2010-10-02 11:55:12 -0700

    the start of the mob class

---
+++ src/mob.js
@@ -0,0 +1,15 @@
+// mob.js
+//
+// Movable things that can collide with other things. Entities have a position
+// in world space and a velocity. Entities do not handle their own collision
+// detection.
+
+(function(global, $) {
+
+  var Mob = function() {
+    
+  };
+  
+  global.Mob = Mob;
+
+})(window, jQuery)
