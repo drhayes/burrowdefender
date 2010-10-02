5a62dd496f17084a383fb3fc03609b62f0ba1e22
David Hayes <drhayes@gmail.com>
2010-10-02 01:00:54 -0700

    beginning work on the tilemanager

--- src/tilemanager.js
+++ src/tilemanager.js
@@ -2,10 +2,24 @@
 //
 // Manages a group of tiles retrievable by tile x,y.
 // This is not the same as entity x,y -- less granular.
+//
+// Depends: tile.js
 
 (function(global, $) {
   
   var TileManager = function() {
+    this.makekey = function(x, y) {
+      return x + ':' + y;
+    };
+    
+    this.get = function() {
+      
+    };
+    
+    this.set = function() {
+      
+    };
+    
   };
   
   global.TileManager = TileManager;
