709990071e8470b0e83c708b0982a6a615114986
David Hayes <drhayes@gmail.com>
2010-10-03 12:53:56 -0700

    spatialhash beginnings

--- src/spatialhash.js
+++ src/spatialhash.js
@@ -3,13 +3,31 @@
 // Keeps track of what we should check collisions against by partitioning
 // worldspace into a grid. Every object that intersects any cell is added to
 // a list corresponding to that cell.
+// Takes world coordinates converts it to an internal representation.
+//
+// Depends: tile.js
 
 (function(global, $) {
 
+  var cellsize = Tile.tilesize * 2;
+
   var SpatialHash = function() {
+    this.spacemap = {};
     
+    this.makekey = function() {
+      
+    };
+    
+    // Given in world coordinates.
+    this.get = function(x, y) {
+      return [];
+    };
+    
+    this.set = function() {
+      
+    };
   };
 
-  window.SpatialHash = SpatialHash;
+  global.SpatialHash = SpatialHash;
 
 })(window, jQuery)
