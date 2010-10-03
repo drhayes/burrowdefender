62e74cba29a3a4b8059d1f4c2c9f27a0b965e655
David Hayes <drhayes@gmail.com>
2010-10-03 14:51:41 -0700

    mob ctor now takes options

--- src/mob.js
+++ src/mob.js
@@ -6,16 +6,34 @@
 
 (function(global, $) {
 
-  var Mob = function() {
-    this.x = 0;
-    this.y = 0;
-    this.size = {
+  var defaults = {
+    x: 0,
+    y: 0,
+    size: {
       x: 16,
       y: 16
-    };
-    this.vel = {
+    },
+    vel: {
       x: 0,
       y: 0
+    }
+  };
+
+  var Mob = function(options) {
+    options = $.extend({}, defaults, options);
+    this.x = options.x;
+    this.y = options.y;
+    this.size = {
+      x: options.size.x,
+      y: options.size.y
+    };
+    this.vel = {
+      x: options.vel.x,
+      y: options.vel.y
+    };
+    
+    this.newposition = function() {
+      
     };
   };
   
