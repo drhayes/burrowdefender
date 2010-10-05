e8954dd9b3cef37888a0d632abdfe8e6cd747de7
David Hayes <drhayes@gmail.com>
2010-10-04 22:21:53 -0700

    tilemaps can draw themselves

--- src/tilemap.js
+++ src/tilemap.js
@@ -9,13 +9,9 @@
   
   var TileMap = function() {
     this.tilemap = {};
-
-    this.makekey = function(x, y) {
-      return x + ':' + y;
-    };
     
     this.get = function(x, y) {
-      var key = this.makekey(x, y);
+      var key = TileMap.makekey(x, y);
       if (key in this.tilemap) {
         return this.tilemap[key];
       }
@@ -23,10 +19,30 @@
     };
     
     this.set = function(x, y, tile) {
-      this.tilemap[this.makekey(x, y)] = tile;
+      this.tilemap[TileMap.makekey(x, y)] = tile;
     };
     
+    // given the 2d context thing that comes with a canvas
+    this.draw = function(ctx) {
+      var tilemap = this;
+      $.each(this.tilemap, function(key, tile) {
+        var pos = TileMap.parsekey(key);
+        tile.draw(pos.x * Tile.tilesize, pos.y * Tile.tilesize, ctx);
+      })
+    };
   };
+  
+  TileMap.makekey = function(x, y) {
+    return x + ':' + y;
+  };
+  
+  TileMap.parsekey = function(key) {
+    var nums = key.split(':');
+    return {
+      x: nums[0],
+      y: nums[1]
+    };
+  }
   
   global.TileMap = TileMap;
   
