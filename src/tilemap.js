226ddc3f2af1a6992ebfde5cdda523883380953a
David Hayes <drhayes@gmail.com>
2010-10-07 22:22:50 -0700

    tilesize aligned scrolling seems to be working

--- src/tilemap.js
+++ src/tilemap.js
@@ -14,23 +14,33 @@
     
     this.get = function(x, y) {
       var key = TileMap.makekey(x, y);
-      if (key in this.tilemap) {
+      if (this.tilemap.hasOwnProperty(key)) {
         return this.tilemap[key];
       }
-      return Tile.AirTile;
+      return Tile.Air;
     };
     
     this.set = function(x, y, tile) {
       this.tilemap[TileMap.makekey(x, y)] = tile;
     };
     
-    // given the 2d context thing that comes with a canvas
-    this.draw = function(ctx) {
-      var tilemap = this;
-      $.each(this.tilemap, function(key, tile) {
-        var pos = TileMap.parsekey(key);
-        tile.draw(pos.x * Tile.tilesize, pos.y * Tile.tilesize, ctx);
-      })
+    // given the 2d context thing that comes with a canvas.
+    // the offsets are given in pixels and are optional.
+    this.draw = function(ctx, offsetx, offsety) {
+      offsetx = offsetx || 0;
+      offsety = offsety || 0;
+      var startx = Math.floor(offsetx / Tile.tilesize);
+      var starty = Math.floor(offsety / Tile.tilesize);
+      var endx = Math.floor((offsetx + this.width) / Tile.tilesize) + 1;
+      var endy = Math.floor((offsety + this.height) / Tile.tilesize) + 1;
+      for (var x = startx; x < endx; x++) {
+        for (var y = starty; y < endy; y++) {
+          var tile = this.get(x, y);
+          var tilex = (x - startx) * Tile.tilesize;
+          var tiley = (y - starty) * Tile.tilesize;
+          tile.draw(tilex, tiley, ctx);
+        }
+      }
     };
   };
   
