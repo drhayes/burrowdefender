ffc6d9cbe48ad35846f88802f41b4aa2d42f7cc1
David Hayes <drhayes@gmail.com>
2010-10-10 21:22:38 -0700

    more definition of the game object

--- src/game.js
+++ src/game.js
@@ -11,6 +11,10 @@
     this.ctx = this.canvas.getContext('2d');
     this.width = this.canvas.width;
     this.height = this.canvas.height;
+    this.tilemap = new TileMap(this.width, this.height);
+    this.spatialhash = new SpatialHash();
+    this.tilegenerator = new TileGenerator(this.tilemap, this.spatialhash);
+    this.keyboardmanager = new KeyboardManager();
   };
   
   global.Game = Game;
