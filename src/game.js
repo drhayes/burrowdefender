f7b94a425ed8d84ff44850891c5196b0a11670cc
David Hayes <drhayes@gmail.com>
2010-10-10 22:25:12 -0700

    game.update does stuff now

--- src/game.js
+++ src/game.js
@@ -20,11 +20,55 @@
       y: this.height / 2
     };
     this.updater = new Updater();
+    this.drawables = [];
+    this.movables = [];
+    this.tickables = [];
+    this.others = [];
     
     // methods
     this.start = function() {
       this.updater.start();
-    }
+    };
+    
+    var me = this;
+    
+    // Whatever this thing is, add it to the appropriate update buckets. If it
+    // can draw itself, add to drawables. If it ticks, add it there. Etc...
+    this.add = function(thing) {
+      if (typeof(thing.tick) === 'function') {
+        this.tickables.push(function() {
+          thing.tick();
+        });
+      };
+      if (typeof(thing.move) === 'function') {
+        this.movables.push(function() {
+          thing.move();
+        });
+      };
+      if (typeof(thing.draw) === 'function') {
+        this.drawables.push(function() {
+          thing.draw(me.ctx);
+        });
+      };
+      if (typeof(thing) === 'function') {
+        this.others.push(thing);
+      };
+    };
+    
+    this.update = function() {
+      $.each(this.tickables, function(index, tickable) {
+        tickable();
+      });
+      $.each(this.movables, function(index, movable) {
+        movable();
+      });
+      $.each(this.drawables, function(index, drawable) {
+        drawable();
+      });
+      $.each(this.others, function(index, other) {
+        other();
+      });
+    };
   };
   
   global.Game = Game;
