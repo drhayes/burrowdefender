b900f113a96e7236db1a9014140d9c2a281b3f94
David Hayes <drhayes@gmail.com>
2010-10-11 08:49:35 -0700

    tick works but is barely tested pending further refactoring

--- src/player.js
+++ src/player.js
@@ -12,6 +12,54 @@
 			// draw a little bigger than player size so player is standing on ground
 			ctx.fillRect(game.playeroffset.x, game.playeroffset.y, this.size.x + 1, this.size.y + 1);
     };
+    
+		var lastmined = 0;
+
+		// update the player every tick
+		this.tick = function() {
+		// gravity has to have some effect here...
+			Mob.gravitytick.call(this);
+			// are we mining?
+			if (game.keyboardmanager.keymap['shift']) {
+				// convert player's current position to tile
+				// respect player's center of mass
+				var tilepos = Tile.totilepos(this.x + this.size.x / 2, this.y + this.size.y / 2);
+				if (game.keyboardmanager.keymap['a']) { // left
+					tilepos.x -= 1;
+				}
+				else if (game.keyboardmanager.keymap['d']) { // right
+					tilepos.x += 1;
+				}
+				else if (game.keyboardmanager.keymap['w']) { // up
+					tilepos.y -= 1;
+				}
+				else if (game.keyboardmanager.keymap['s']) { // down
+					tilepos.y += 1;
+				}
+				// is it a dirt tile?
+				var digtile = game.tilemap.get(tilepos.x, tilepos.y);
+				if (digtile === Tile.Dirt || digtile === Tile.DirtWithGrass) {
+					var currenttime = new Date().getTime();
+					if (currenttime - lastmined > 500) {
+						game.tilemap.set(tilepos.x, tilepos.y, Tile.DirtDug);
+						game.spatialhash.remove(TileMap.getrect(tilepos.x, tilepos.y));
+						lastmined = new Date().getTime();
+					}
+				}
+			}
+			if (game.keyboardmanager.keymap['a']) {
+				this.vel.x = -2;
+			}
+			else if (game.keyboardmanager.keymap['d']) {
+				this.vel.x = 2;
+			}
+			else {
+				this.vel.x = 0;
+			}
+			if (game.keyboardmanager.keymap['w']) {
+				this.jump();
+			}
+		}
   };
   
   Player.prototype = new Mob({
