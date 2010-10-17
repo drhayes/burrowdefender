07bf6a39e0a4390c6d9fa1d0bad11e7a736ba1a3
David Hayes <drhayes@gmail.com>
2010-10-17 15:33:01 -0700

    game converted to functional instead of new-based

--- src/game.js
+++ src/game.js
@@ -6,51 +6,50 @@
 
 (function(global, $) {
   
-  var Game = function(canvasid) {
-    this.canvas = $(canvasid)[0];
-    this.ctx = new DrawContext(this.canvas.getContext('2d'));
-    this.width = this.canvas.width;
-    this.height = this.canvas.height;
-    this.tilemap = new TileMap(this.width, this.height);
-    this.spatialhash = new SpatialHash();
-    this.tilegenerator = new TileGenerator(this);
-    this.keyboardmanager = new KeyboardManager();
-    this.playeroffset = {
-      x: this.width / 2,
-      y: this.height / 2
+  var game = function(canvasid) {
+    var that = {};
+    that.canvas = $(canvasid)[0];
+    that.ctx = new DrawContext(that.canvas.getContext('2d'));
+    that.width = that.canvas.width;
+    that.height = that.canvas.height;
+    that.tilemap = new TileMap(that.width, that.height);
+    that.spatialhash = new SpatialHash();
+    that.tilegenerator = new TileGenerator(that);
+    that.keyboardmanager = new KeyboardManager();
+    that.playeroffset = {
+      x: that.width / 2,
+      y: that.height / 2
     };
-    this.updater = new Updater();
-    this.things = [];
-    this.addthings = [];
+    that.updater = new Updater();
+    that.things = [];
+    that.addthings = [];
     
     // temporary tile generation
-		this.tilegenerator.generate(31 * Tile.tilesize, 0);
-		this.tilegenerator.generate(0, 0);
-		this.tilegenerator.generate(-10 * Tile.tilesize, 0);
+		that.tilegenerator.generate(31 * Tile.tilesize, 0);
+		that.tilegenerator.generate(0, 0);
+		that.tilegenerator.generate(-10 * Tile.tilesize, 0);
     
     // methods
-    this.start = function() {
-      this.updater.start();
+    that.start = function() {
+      that.updater.start();
     };
-    
-    var me = this;
     
     // Whatever this thing is, add it to the appropriate update buckets. If it
     // can draw itself, add to drawables. If it ticks, add it there. Etc...
-    this.add = function(thing) {
-      this.addthings.push(thing);
+    that.add = function(thing) {
+      that.addthings.push(thing);
     };
     
-    this.update = function() {
+    that.update = function() {
       // update the tilemap
-      this.tilemap.tick();
+      that.tilemap.tick();
       // things can kill themselves on tick, so we need a new list of the
       // unkilled things after each update cycle...
       var newthings = [];
       // gather the drawables
-      this.drawables = [];
+      that.drawables = [];
       // run all the updates on the things
-      $.each(this.things, function(index, thing) {
+      $.each(that.things, function(index, thing) {
         if (typeof(thing) === 'function') {
           thing();
         };
@@ -58,38 +57,39 @@
           thing.tick();
         };
         if (thing.move) {
-          thing.move(me.spatialhash.get(thing, thing.vel.x, thing.vel.y));
+          thing.move(that.spatialhash.get(thing, thing.vel.x, thing.vel.y));
         };
         if (thing.draw) {
-          me.drawables.push(function(drawthing) {
+          that.drawables.push(function(drawthing) {
             thing.draw(drawthing);
           });
         };
-        if (typeof(thing.killed) !== 'undefined' || !thing.killed) {
+        if (typeof(thing.killed) === 'undefined' || !thing.killed) {
+          console.log('not killed');
           newthings.push(thing);
         }
       });
       // swap the list if anything was killed and add anything that was added
       // during the update cycle.
-      this.things = newthings;
-      if (this.addthings.length > 0) {
-        this.things = newthings.concat(this.addthings);
+      that.things = newthings;
+      if (that.addthings.length > 0) {
+        that.things = newthings.concat(that.addthings);
       }
-      this.addthings = [];
+      that.addthings = [];
     };
     
     var drawiterate = function(drawthing, layer) {
       var drawthings = drawthing[layer];
       $.each(drawthings, function(i, thing) {
-        me.ctx.offset = {
-          x: -(me.player.x - me.playeroffset.x),
-          y: -(me.player.y - me.playeroffset.y)
+        that.ctx.offset = {
+          x: -(that.player.x - that.playeroffset.x),
+          y: -(that.player.y - that.playeroffset.y)
         };
-        thing(me.ctx);
+        thing(that.ctx);
       });
     };
     
-    this.draw = function() {
+    that.draw = function() {
       var drawthing = {
         background: [],
         tile: [],
@@ -98,7 +98,7 @@
         hud: []
       };
       // get all the drawable functions from people
-      $.each(this.drawables, function(i, drawable) {
+      $.each(that.drawables, function(i, drawable) {
         drawable(drawthing);
       });
       drawiterate(drawthing, 'background');
@@ -109,47 +109,49 @@
     };
     
     // add the update method to the updater thingy
-    this.updater.add(function() {
-      me.update();
+    that.updater.add(function() {
+      that.update();
     });
     // add the draw method to the updater thing
-    this.updater.add(function() {
-      me.draw();
+    that.updater.add(function() {
+      that.draw();
     });
     
-    this.clearbackground = function(ctx) {
+    that.clearbackground = function(ctx) {
       ctx.offset = {
         x: 0,
         y: 0
       };
 			ctx.fillStyle('rgb(255, 255, 255)');
-			ctx.fillRect(0, 0, this.width, this.height);
+			ctx.fillRect(0, 0, that.width, that.height);
     };
     
-    this.drawtiles = function(ctx) {
-      this.tilemap.draw(ctx, this.player.x - this.playeroffset.x, this.player.y - this.playeroffset.y);
+    that.drawtiles = function(ctx) {
+      that.tilemap.draw(ctx, that.player.x - that.playeroffset.x, that.player.y - that.playeroffset.y);
     };
     
     // add our drawing tasks
     var backgrounddraw = {
       draw: function(drawthing) {
         drawthing.background.push(function(ctx) {
-          me.clearbackground(ctx);
+          that.clearbackground(ctx);
         });
         drawthing.tile.push(function(ctx) {
-          me.drawtiles(ctx);
+          that.drawtiles(ctx);
         });
       }
     };
-    this.add(backgrounddraw);
+    that.add(backgrounddraw);
     
     // last but not least, add the player
-    this.player = new Player(this);
-    this.player.x = 0;
-    this.player.y = -200;
-    this.add(this.player);
+    that.player = new Player(that);
+    that.player.x = 0;
+    that.player.y = -200;
+    that.add(that.player);
+    
+    return that;
   };
   
-  global.Game = Game;
+  global.game = game;
   
 })(window, jQuery)
