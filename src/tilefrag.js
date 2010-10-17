06fc5bf24700e8a61c54a4926339defa98147848
David Hayes <drhayes@gmail.com>
2010-10-17 16:57:43 -0700

    updated tilefrag, though nobody cares right now

--- src/tilefrag.js
+++ src/tilefrag.js
@@ -7,39 +7,38 @@
 
 (function(global, $) {
   
-  var TileFrag = function(game) {
-    var me = this;
-    this.game = game;
-    this.created = new Date().getTime();
-    this.size = {
+  var tilefrag = function(args) {
+    var that = mob(args);
+    that.created = new Date().getTime();
+    that.size = {
       x: 5,
       y: 5
     };
-    this.vel = {
+    that.vel = {
       x: Math.round(Math.random() * 4) - 2,
-      y: 1
+      y: -1
     };
     
-    this.tick = function() {
+    that.tick = function() {
       // under the effect of gravity
-      Mob.gravitytick.call(this);
+      mob.gravitytick.call(this);
       // disappears after a few seconds
       var current = new Date().getTime();
-      if (current - this.created > 3000) {
-        this.killed = true;
+      if (current - that.created > 3000) {
+        that.killed = true;
       }
     };
     
-    this.draw = function(drawthing) {
+    that.draw = function(drawthing) {
       drawthing.sprite2.push(function(ctx) {
         ctx.fillStyle('rgb(192, 192, 192)');
-        ctx.fillRect(me.x, me.y, me.size.x, me.size.y);
+        ctx.fillRect(that.x, that.y, that.size.x, that.size.y);
       });
     };
     
+    return that;
   };
-  TileFrag.prototype = new Mob();
   
-  global.TileFrag = TileFrag;
+  global.tilefrag = tilefrag;
   
 })(window, jQuery);
