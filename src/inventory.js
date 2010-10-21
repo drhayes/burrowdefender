4465d8cbf1379fcbfe388f5b49dc56af56c5d840
David Hayes <drhayes@gmail.com>
2010-10-20 23:36:20 -0700

    adding things of different types works

--- src/inventory.js
+++ src/inventory.js
@@ -4,22 +4,35 @@
 
 (function(global, $) {
   
+  // maintains a 1-based map of things the player is carrying. this maps neatly
+  // to the scheme of getting the player to press number keys to specify
+  // a particular spot in the player's inventory.
   var inventory = function(options) {
     var that = {};
     
     that.things = {};
     
+    var thingtospotmap = {};
+    var nextindex = 1;
+    
     // thing is a constant representing something that can be picked up in
     // the game world. add will find the first available inventory slot
     // for this type of thing and will add it to the count found there.
     that.add = function(thing) {
-      if (!that.things.hasOwnProperty('0')) {
-        that.things['0'] = {
+      // find the index for this thing
+      if (!thingtospotmap.hasOwnProperty(thing)) {
+        // this must be a string
+        thingtospotmap[thing] = '' + nextindex;
+        nextindex++;
+      }
+      var spot = thingtospotmap[thing];
+      if (!that.things.hasOwnProperty(spot)) {
+        that.things[spot] = {
           type: thing,
           count: 0
         };
       };
-      that.things['0'].count += 1;
+      that.things[spot].count += 1;
     };
     
     return that;
