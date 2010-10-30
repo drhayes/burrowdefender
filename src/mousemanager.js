58a0c3daedfe704626ae2c52495097f593a34384
David Hayes <drhayes@gmail.com>
2010-10-29 22:43:29 -0700

    more mousemanager stuff

--- src/mousemanager.js
+++ src/mousemanager.js
@@ -8,6 +8,15 @@
   var mousemanager = function(args) {
     var that = {};
     
+    // mouse position exposed
+    that.pos = {
+      x: 0,
+      y: 0
+    };
+    
+    // grab the canvas offsets...
+    var canvasoffset = $(args.game.canvas).offset();
+    
     var dispatchevent = function(e) {
       that.dispatch(e);
     }
@@ -19,6 +28,14 @@
     that.unlatch = function() {
       $(args.game.canvas).unbind(mouseevents, dispatchevent);
     };
+    
+    that.dispatch = function(e) {
+      // handle mouse movement
+      that.pos.x = e.pageX - canvasoffset.left;
+      that.pos.y = e.pageY - canvasoffset.top;
+    };
+    
+    that.latch();
     
     return that;
   };
