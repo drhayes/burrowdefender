428845d2f25289391b90c609faae2194b3cbbae6
David Hayes <drhayes@gmail.com>
2010-10-29 21:16:03 -0700

    beginning of mousemanager stuff

---
+++ src/mousemanager.js
@@ -0,0 +1,22 @@
+// mousemanager.js
+//
+// Fires events relating to mouse stuff.
+
+(function(global, $) {
+  var mousemanager = function() {
+    var that = {};
+    
+    that.latch = function() {
+      
+    };
+    
+    that.unlatch = function() {
+      
+    };
+    
+    return that;
+  };
+
+  global.mousemanager = mousemanager;
+
+})(window, jQuery)
