47d3763a0f74dfd0444cba26b39caac69e61be5f
David Hayes <drhayes@gmail.com>
2010-10-30 12:38:41 -0700

    got the reticle rectangle working

--- src/mininginterface.js
+++ src/mininginterface.js
@@ -12,19 +12,24 @@
     // returns a rect based on the current mouse position aligned with the
     // offset tiles underneath.
     that.reticlerect = function() {
-      // read the mouse position and clamp to tile dimensions
-      var tilepos = tile.totilepos(
-        args.game.mousemanager.pos.x - args.game.tilemap.offset.x,
-        args.game.mousemanager.pos.y - args.game.tilemap.offset.y);
+      // clamp to tile coordinates
+      var t = tile.totilepos(
+        args.game.mousemanager.pos.x + args.game.worldoffset.x,
+        args.game.mousemanager.pos.y + args.game.worldoffset.y);
       return {
-        x1: tilepos.x * tile.tilesize,
-        y1: tilepos.y * tile.tilesize,
-        x2: tilepos.x * tile.tilesize + tile.tilesize,
-        y2: tilepos.y * tile.tilesize + tile.tilesize
+        x1: (t.x * tile.tilesize) - args.game.worldoffset.x,
+        y1: (t.y * tile.tilesize) - args.game.worldoffset.y,
+        x2: (t.x * tile.tilesize) - args.game.worldoffset.x + tile.tilesize,
+        y2: (t.y * tile.tilesize) - args.game.worldoffset.y + tile.tilesize
       };
     }
     
     that.draw = function(drawthing) {
+      drawthing.hud.push(function(ctx) {
+        var rect = that.reticlerect();
+        ctx.strokeStyle('hsla(120, 80%, 50%, 0.6)');
+        ctx.strokeRect(rect.x1, rect.y1, rect.x2 - rect.x1, rect.y2 - rect.y1);
+      })
     }
     
     return that;
