0afca00b9556bf144f19102de35717a7d583bed2
David Hayes <drhayes@gmail.com>
2010-10-30 11:10:50 -0700

    beginnings of the mining interface

---
+++ src/mininginterface.js
@@ -0,0 +1,20 @@
+// mininginterface.js
+//
+// Mouse-based interface to let the player dig and place items.
+//
+// Depends: tilemap.js
+
+(function(global, $) {
+  
+  var mininginterface = function(args) {
+    var that = {};
+    
+    that.draw = function(drawthing) {
+    }
+    
+    return that;
+  };
+  
+  global.mininginterface = mininginterface;
+  
+})(window, jQuery)
