e6928f8d29c928b8a8f1fc73e3f97800e5fb350c
David Hayes <drhayes@gmail.com>
2010-11-03 23:21:57 -0700

    utils now has damageable component thingamajigger

--- src/utils.js
+++ src/utils.js
@@ -28,6 +28,28 @@
         return false;
       }
       return true;
+    },
+    // pass any object to this method and it will gain a damage method and
+    // attributes suitable for tracking health
+    damageable: function(thing, args) {
+      if (typeof args.health !== 'number') {
+        console.error('args.health required!');
+      }
+      thing.health = args.health;
+      thing.maxhealth = args.health;
+      // is this a something that should have its velocity affected by damage?
+      if (typeof thing.vel === 'object') {
+        thing.damage = function(amt) {
+          thing.health -= amt;
+          thing.vel.x = -4;
+          thing.vel.y = -7;
+        }
+      }
+      else {
+        thing.damage = function(amt) {
+          thing.health -= amt;
+        }
+      }
     }
   }
   
