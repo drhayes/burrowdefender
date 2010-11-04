f2e3c0d68ad3e467f62b5f948cc53eed8e960616
David Hayes <drhayes@gmail.com>
2010-11-03 23:34:35 -0700

    a bullet!

--- src/defenses.js
+++ src/defenses.js
@@ -8,6 +8,23 @@
   
   var defenses = {};
   
+  defenses.bullet = function(args) {
+    var that = mob(args);
+    that.size = {
+      x: 2,
+      y: 2
+    };
+    
+    that.draw = function(drawthing) {
+      drawthing.sprite1.push(function(ctx) {
+        ctx.fillStyle('hsl(120, 100%, 100%)');
+        ctx.fillRect(that.x, that.y, 2, 2);
+      });
+    }
+    
+    return that;
+  }
+  
   defenses.sentrygun = function(args) {
     var that = mob(args);
     
