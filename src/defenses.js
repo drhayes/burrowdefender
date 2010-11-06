fde36bdeca32af66bc1444cff1344478c14c316c
David Hayes <drhayes@gmail.com>
2010-11-06 00:14:32 -0700

    bullet is killed after one second

--- src/defenses.js
+++ src/defenses.js
@@ -15,6 +15,9 @@
       y: 4
     };
     
+    // used to kill the bullet after it has lived too long
+    that.created = new Date().getTime();
+    
     that.draw = function(drawthing) {
       drawthing.sprite1.push(function(ctx) {
         ctx.fillStyle('hsl(120, 100%, 100%)');
@@ -29,6 +32,14 @@
       collider.damage(7);
       // remove the bullet from the game... its dread business is done
       that.killed = true;
+    };
+    
+    // kill the bullet after a second or so
+    that.tick = function() {
+      var current = new Date().getTime();
+      if (current - that.created > 1000) {
+        that.killed = true;
+      }
     };
     
     return that;
