4cb273b91005131e8226067ce1d325d27d589229
David Hayes <drhayes@gmail.com>
2010-11-13 00:16:58 -0800

    constants module

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+// constants.js
+//
+// Constants used throughout the game.
+
+(function(global, $) {
+  
+  loki.modules.constants = function(env) {
+    env.tilesize = 32;
+  }
+  
+}(this, jQuery));
