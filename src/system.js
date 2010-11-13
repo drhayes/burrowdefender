7c834bf9b50121283e6191a011223fc731ae5882
David Hayes <drhayes@gmail.com>
2010-11-12 23:54:36 -0800

    eventer is now part of system module

--- src/system.js
+++ src/system.js
@@ -51,7 +51,45 @@
       };
       
       return that;
-    }
+    }; // updater
+    
+    env.eventer = function() {
+      var that = {};
+      var subscribers = {};
+
+      that.subscribe = function(eventName, callback) {
+          if (eventName === null) {
+              throw new Error('eventName required');
+          }
+          if (typeof(callback) !== 'function') {
+              throw new Error('callback must be function');
+          }
+          var callbacks = subscribers[eventName];
+          if (typeof(callbacks) === 'undefined') {
+              callbacks = [];
+          }
+          callbacks.push(callback);
+          subscribers[eventName] = callbacks;
+      }
+
+      that.fire = function(eventName, eventArg) {
+          if (eventName === null) {
+              throw new Error('eventName required');
+          }
+          // if no subscribers for an event, exit early
+          if (!subscribers.hasOwnProperty(eventName)) {
+              return;
+          }
+          var callbacks = subscribers[eventName];
+          if (callbacks !== null) {
+              $.each(callbacks, function(index, callback) {
+                  callback(eventArg);
+              });
+          };
+      };
+      
+      return that;
+    }; // eventer
   }
   
 }(this, jQuery));
