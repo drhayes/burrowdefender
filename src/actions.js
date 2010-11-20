f010636815ce47df2100b53d8ed2ef705a010533
David Hayes <drhayes@gmail.com>
2010-11-19 21:50:00 -0800

    beginnings of an action framework

---
+++ src/actions.js
@@ -0,0 +1,15 @@
+// actions.js
+//
+// Framework for all things actiony.
+
+(function(global, $) {
+  
+  loki.modules.actions = function(env) {
+    env.actionmanager = function() {
+      var that = {};
+      
+      return that;
+    }
+  };
+  
+}(this, jQuery));
