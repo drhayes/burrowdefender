6b37336ebc9ded435bb2b64570e1db2733c24303
David Hayes <drhayes@gmail.com>
2010-11-20 01:15:39 -0800

    actions are queued and can determine if they can combine with anything else

--- src/actions.js
+++ src/actions.js
@@ -20,15 +20,36 @@
         return that;
       };
       
+      // highest priority actions are run first
       env.actionmanager = function() {
-        var that = {};
-        var active = pqueue();
+        var that = {},
+          active = pqueue(),
+          queue = pqueue();
         
         that.add = function(a) {
-          active.push(a);
+          queue.push(a);
         };
         
         that.execute = function() {
+          // add actions from the queue
+          var queuecopy = queue.slice(0);
+          var offset = 0;
+          for (var i = 0, queuelength = queuecopy.length; i < queuelength; i++) {
+            var action = queuecopy[i];
+            // can this action combine with the actives?
+            var addit = true;
+            for (var j = active.length - 1, activeaction; activeaction = active[j--];) {
+              addit &= activeaction.candoboth(action);
+            }
+            // if this action can't combine, skip it for now
+            if (!addit) {
+              continue;
+            }
+            // remove from queue and add to active
+            queue.splice(i - offset, 1);
+            offset++;
+            active.push(action);
+          }
           // copy the active list as we might have to remove completed actions
           var activecopy = active.slice(0);
           // execute the active actions
