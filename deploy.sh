0fc0ae33f9a5d151761777dfbecdf96fd4986941
David Hayes <drhayes@gmail.com>
2010-12-04 11:09:21 -0800

    trying to get commit working

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,6 @@
 
 echo 'Deploying Mine Defender...'
 git checkout gh-pages && git checkout master index.html lib/ src/ assets/
+echo 'Committing changes...'
+var DATE=`date`
+git commit -am "Update to Mine Defender for $DATE"
