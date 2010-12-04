8b8a81935e61791ff56a3b5528e9f3cd6100d31d
David Hayes <drhayes@gmail.com>
2010-12-04 11:10:52 -0800

    I think I am finished

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,7 @@ git checkout gh-pages && git checkout master index.html lib/ src/ assets/
 echo 'Committing changes...'
 var DATE=`date`
 git commit -am "Update to Mine Defender for $DATE"
+echo 'Pushing changes...'
+git push origin gh-pages
+echo 'Switching back to master...'
+git checkout master
