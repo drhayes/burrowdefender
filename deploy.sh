df605e7ea893cce318c38f011123e126b37edc66
David Hayes <drhayes@gmail.com>
2010-12-10 23:36:16 -0800

    changed deploy script to use scp to own domain instead

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 echo 'Deploying Mine Defender...'
-git checkout gh-pages && git checkout master index.html lib/ src/ assets/
-echo 'Committing changes...'
-var DATE=`date`
-git commit -am "Update to Mine Defender for $DATE"
-echo 'Pushing changes...'
-git push origin gh-pages
-echo 'Switching back to master...'
-git checkout master
+mkdir deploy
+echo 'Copying files to temp deploy directory...'
+cp index.html deploy/index.html
+cp -R lib/ deploy/lib/
+cp -R assets deploy/assets/
+cp -R src deploy/src/
+echo 'Copying files to remote server...'
+scp deploy/index.html minedefendernfs:/home/public/index.html
+scp deploy/lib/ minedefendernfs:/home/public/lib/
+scp deploy/assets/ minedefendernfs:/home/public/assets/
+scp deploy/src/ minedefendernfs:/home/public/src/
+echo 'Removing temp deploy directory...'
+rm -rf deploy/
+echo 'Done.'
