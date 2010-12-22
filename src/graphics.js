e4036f67b41afc88471511a3f1a798c2b41e5380
David Hayes <drhayes@gmail.com>
2010-12-21 23:00:56 -0800

    got sequence working

--- src/graphics.js
+++ src/graphics.js
@@ -8,12 +8,25 @@
     var spritemanager = env.spritemanager;
     
     loki.modules.graphics = function(env) {
+      // run the following frames in sequence
+      env.sequence = function(frames) {
+        var index = 0;
+        return function(ctx, name, x, y) {
+          spritemanager.draw(ctx, name, frames[index], x, y);
+          index += 1;
+          return index === frames.length;
+        }
+      };
+
+      // repeat the given frames some number of times randomly chosen between
+      // lo inclusive and hi exclusive.
       env.repeater = function(frames, lo, hi) {
+        var target = hi ? Math.floor(Math.random() * (hi - lo)) + lo : lo;
         var count = 0;
         return function(ctx, name, x, y) {
           spritemanager.draw(ctx, name, 0, x, y);
           count += 1;
-          return (count === lo);
+          return (count === target);
         }
       };
 
